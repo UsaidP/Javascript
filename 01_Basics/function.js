f9336e473f29d7b1cab1ab1207812aeea9aecb07
Usaid Patel <71535693+UsaidP@users.noreply.github.com>
2023-12-02 15:17:46 +0000

    Function Basic

---
+++ 01_Basics/function.js
@@ -0,0 +1,22 @@
+// function basic (){
+//     console.log("Usaid")
+//     return
+// }
+// basic()
+
+// function addition (num1 , num2){
+//     return console.log(num1*num2)
+// }
+// addition(6,7)
+
+
+// function FormSubmit(form1){
+//     if (form1 === undefined) {
+//         return
+//     }
+//     else
+//     return console.log(`Form ${form1} Succesfully!`)
+// }
+// FormSubmit()
+
+ 
